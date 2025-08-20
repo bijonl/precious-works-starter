1e981d71e33598c5cbd6475ddc1c98c119118139
bijonl <bijon2591@gmail.com>
2025-08-20 12:51:09 -0400

    removing font-awesome

--- precious-works-theme-child/functions.php
+++ precious-works-theme-child/functions.php
@@ -9,11 +9,10 @@ define( 'PW_THEME_CHILD_VERSION', $theme->get( 'Version' ) );
 
 
 function pw_enqueue_scripts() {
-    // Enqueue parent style first
     wp_enqueue_style( 'parent-style', get_stylesheet_directory_uri() . '/assets/dist/css/style.min.css', [], PW_THEME_CHILD_VERSION );
 
     // Then enqueue child style, dependent on parent-style
-    wp_enqueue_style( 'pw-style', get_stylesheet_directory_uri() . '/assets/dist/css/style.min.css', ['parent-style'], PW_THEME_CHILD_VERSION );
+    // wp_enqueue_style( 'pw-style', get_stylesheet_directory_uri() . '/assets/dist/css/style.min.css', ['parent-style'], PW_THEME_CHILD_VERSION );
     
     // JS if needed
     wp_enqueue_script( 'pw-main', get_template_directory_uri()  . '/assets/js/main.js', [], PW_THEME_CHILD_VERSION, true );
